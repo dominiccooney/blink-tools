098b25271ac472b781e67f566de2b25c37a5c3ad
Dominic Cooney <dominicc@chromium.org>
2013-08-01 09:40:28 +0900

    Rename wk- to bk- in honor of Blink.

--- blink-stuff.el
+++ blink-stuff.el
@@ -1,61 +1,61 @@
 ;;
-;; This has two useful functions for working with WebKit in emacs:
+;; This has functions for working with Blink in emacs:
 ;;
-;; wk-find-binding-files finds files related to JavaScript bindings
+;; bk-find-binding-files finds files related to JavaScript bindings
 ;; for the current buffer; for example, when visiting Node.h if you
-;; M-x wk-find-binding-files it will search your source tree and
-;; present you with a menu including Node.cpp, Node.idl, JSC and V8
-;; custom bindings and JSC and V8 generated bindings (if they exist in
-;; your build output.)
+;; M-x bk-find-binding-files it will search your source tree and
+;; present you with a menu including Node.cpp, Node.idl, V8 custom
+;; bindings and V8 generated bindings (if they exist in your build
+;; output.)
 ;;
-;; wk-refresh-files is for getting your open buffers to a good state
+;; bk-refresh-files is for getting your open buffers to a good state
 ;; after a git rebase. It looks at all of your open buffers and, if
 ;; they haven't been edited, updates them to the latest on-disk
-;; versions; if they have been edited wk-refresh-files will prompt you
+;; versions; if they have been edited bk-refresh-files will prompt you
 ;; to revert them or not (or you can use C-g to bail out at this
 ;; unhappy juncture to save them and use git diff to clean up.)
 ;;
 ;; This also has some mode hooks and font tweaks for highlighting
 ;; tabs. To install the hooks, run:
 ;;
-;;    (wk-setup)
+;;    (bk-setup)
 
-(defun wk-setup ()
+(defun bk-setup ()
   "Sets up various hooks and fonts for Blink development."
   (interactive)
 
-  (add-hook 'c++-mode-hook 'wk-prog-mode-hook)
-  (add-hook 'change-log-mode-hook 'wk-change-log-mode-hook)
+  (add-hook 'c++-mode-hook 'bk-prog-mode-hook)
+  (add-hook 'change-log-mode-hook 'bk-change-log-mode-hook)
 
   (setq auto-mode-alist
         (append (list '("\\.mm\\'" . objc-mode)
                       '("\\.h\\'" . c++-mode))
                 auto-mode-alist))
 
-  (defface wk-tab-face
+  (defface bk-tab-face
     '((t (:background "Red"))) "Tab" :group 'font-lock-faces)
 
-  (defface wk-generated-file-face
+  (defface bk-generated-file-face
     '((t (:foreground "Black"
           :background "color-149"))) "Generated file" :group 'font-lock-faces)
 
   (font-lock-add-keywords 'c++-mode
-                          '(("\\(\t+\\)" 1 'wk-tab-face)))
+                          '(("\\(\t+\\)" 1 'bk-tab-face)))
   (font-lock-add-keywords 'change-log-mode
-                          '(("\\(\t+\\)" 1 'wk-tab-face))))
+                          '(("\\(\t+\\)" 1 'bk-tab-face))))
 
-(defun wk-prog-mode-hook ()
+(defun bk-prog-mode-hook ()
   (if (and (string-match "WebKit" (buffer-file-name)))
       (progn
        (set-variable 'indent-tabs-mode nil)
        (set-variable 'c-basic-offset 4))))
 
-(defun wk-change-log-mode-hook ()
+(defun bk-change-log-mode-hook ()
   (if (string-match "WebKit" (buffer-file-name))
       (let ()
         (set-variable 'indent-tabs-mode nil))))
 
-(defun wk-characterize-path (file-name)
+(defun bk-characterize-path (file-name)
   "Characterizes a file path as V8, or co for core."
   (catch 'return
     (let ((patterns '(("/blink/bindings/" . "V8")
@@ -67,7 +67,7 @@
           (throw 'return (cdr pattern)))))
     "??"))
 
-(defun wk-is-generated-file (file-name)
+(defun bk-is-generated-file (file-name)
   "Returns non-NIL if the specified file is generated."
   (catch 'return
     (let ((patterns '(("/blink/bindings/" . 't)
@@ -81,7 +81,7 @@
     nil))
 
 
-(defun wk-binding-alternatives (root base-name)
+(defun bk-binding-alternatives (root base-name)
   "Gets file paths for a binding BASE-NAME in Blink tree at ROOT."
   (let ((patterns (list
                    ;; core .h, .cpp and .idl
@@ -103,37 +103,37 @@
       (setq result (append (file-expand-wildcards pattern) result)))
     (sort result #'string<)))
 
-(defconst wk-file-list-buffer "*Blink files*"
+(defconst bk-file-list-buffer "*Blink files*"
   "Buffer to use for display lists of Blink files.")
 
-(defun wk-display-file-list (root files)
+(defun bk-display-file-list (root files)
   "Display a list of FILES shortening the names by trimming off ROOT."
   (catch 'return
     (unless files
       (message "no files to display")
       (throw 'return nil))
-    (select-window (or (get-buffer-window wk-file-list-buffer)
+    (select-window (or (get-buffer-window bk-file-list-buffer)
                        (split-window (selected-window))))
     (shrink-window (- (window-height) (+ 2 (length files))))
-    (switch-to-buffer (get-buffer-create wk-file-list-buffer))
+    (switch-to-buffer (get-buffer-create bk-file-list-buffer))
     (erase-buffer)
     (setq files
           (sort files (lambda (p1 p2)
-                        (or (string< (wk-characterize-path p1)
-                                     (wk-characterize-path p2))
+                        (or (string< (bk-characterize-path p1)
+                                     (bk-characterize-path p2))
                             (string< (substring p1 (length root))
                                      (substring p2 (length root)))))))
     (dolist (file files)
       (let ((short-file-path (substring file (length root)))
             (start (point)))
-        (insert (wk-characterize-path file))
-        (if (wk-is-generated-file file)
+        (insert (bk-characterize-path file))
+        (if (bk-is-generated-file file)
             (add-text-properties
              start (point)
-             '(font-lock-face wk-generated-file-face)))
+             '(font-lock-face bk-generated-file-face)))
         (insert " ")
         (insert-text-button short-file-path
-                            'action #'wk-display-file-list-action
+                            'action #'bk-display-file-list-action
                             'button file)
         (insert "\n")))
     (goto-char (point-min))
@@ -141,13 +141,13 @@
     (font-lock-mode 't)
     ))
 
-(defun wk-display-file-list-action (button)
+(defun bk-display-file-list-action (button)
   "Visits a file when it is activated in the file list."
   (let ((path (get-text-property button 'button)))
     (delete-window)
     (find-file path)))
 
-(defun wk-root-base-name-of-file (file)
+(defun bk-root-base-name-of-file (file)
   "Gets the base name of FILE or nil if plausibly a file with a JS binding.
 Returns a pair of `(ROOT . BASE-NAME)' where ROOT is the Blink folder."
   (catch 'return
@@ -180,16 +180,16 @@ Returns a pair of `(ROOT . BASE-NAME)' where ROOT is the Blink folder."
             (throw 'return (cons (funcall mk-path (cdr (assoc 'root (cdr pattern))))
                                  (funcall mk-path (cdr (assoc 'base-name (cdr pattern))))))))))))
 
-(defun wk-find-binding-files ()
+(defun bk-find-binding-files ()
   "Finds JavaScript binding files related to the current buffer."
   (interactive)
-  (let ((root-base-name (wk-root-base-name-of-file (buffer-file-name))))
+  (let ((root-base-name (bk-root-base-name-of-file (buffer-file-name))))
     (if root-base-name
-        (wk-display-file-list (car root-base-name)
-                              (wk-binding-alternatives (car root-base-name) (cdr root-base-name)))
+        (bk-display-file-list (car root-base-name)
+                              (bk-binding-alternatives (car root-base-name) (cdr root-base-name)))
       (message "JavaScript binding files not found"))))
 
-(defun wk-refresh-files ()
+(defun bk-refresh-files ()
   "Refreshes buffers, useful after a git rebase."
   (interactive)
   (save-excursion
@@ -208,7 +208,7 @@ Returns a pair of `(ROOT . BASE-NAME)' where ROOT is the Blink folder."
 
 ;; (setq edebug-all-defs nil)
 
-(provide 'webkit-stuff)
+(provide 'blink-stuff)
 
 ;; TODO: characterize results as [D]ebug or [R]elease
 ;; TODO: color the results buffer instead of using ugly underlined buttons
