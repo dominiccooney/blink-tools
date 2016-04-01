6e4ff9da1c5f522ef46105eb153e3ac6b377eb88
Dominic Cooney <dominicc@chromium.org>
2016-04-01 14:47:25 +0900

    Patch from Windows.

--- roll.py
+++ roll.py
@@ -8,52 +8,60 @@ import subprocess
 #
 # Prerequisites:
 #
-# Check out Chromium somewhere on Linux, Mac and Windows.
-# Check out git://git.gnome.org/libxslt somewhere.
+# - Check out Chromium somewhere on Linux, Mac and Windows.
+# - Add the experimental remote named 'wip'.
+# - Check out git://git.gnome.org/libxslt somewhere.
 
-# Where you have git checkout git://git.gnome.org/libxslt
+third_party_libxslt = 'third_party/libxslt'
+
 libxslt_path = 'libxslt_path'
-
-# Where you store Chromium source on Linux, up to and including src
-linux_src_path = 'src_path'
-
-# The ref of the Experimental Branch to push intermediate steps to,
-# to copy files between platforms.
+src_path_linux = 'src_path_linux'
+src_path_windows = 'src_path_windows'
 wip_ref = 'wip_ref'
 
 # Dominic's default setup.
 config = {
+  # Where you have git checkout git://git.gnome.org/libxslt
   libxslt_path: '/work/xml/libxslt',
-  linux_src_path: '/work/cb/src',
+
+  # Where you store Chromium source, up to and including src
+  src_path_linux: '/work/cb/src',
+  src_path_windows: r'C:\src\ca\src',
+
+  # The ref of the Experimental Branch to push intermediate steps to,
+  # to copy files between platforms.
   wip_ref: 'refs/wip/dominicc/autoroll-libxslt'
 }
+
+def git(*args):
+  command = ['git'] + list(args)
+  # git is a batch file in depot_tools on Windows, so git needs shell=True
+  subprocess.check_call(command, shell=(os.name=='nt'))
 
 def sed_in_place(input_filename, program):
   subprocess.check_call(['sed', '-i', program, input_filename])
 
-def roll_libxslt(config):
-  third_party_libxslt = 'third_party/libxslt'
+def roll_libxslt_linux(config):
   files_to_preserve = ['OWNERS', 'README.chromium', 'BUILD.gn']
 
-  full_path_to_third_party_libxslt = os.path.join(config[linux_src_path],
+  full_path_to_third_party_libxslt = os.path.join(config[src_path_linux],
                                                   third_party_libxslt)
 
-  os.chdir(config[linux_src_path])
+  os.chdir(config[src_path_linux])
 
   # Nuke the old third_party/libxslt from orbit.
-  subprocess.check_call(['git', 'rm', '-rf', third_party_libxslt])
+  git('rm', '-rf', third_party_libxslt)
   shutil.rmtree(third_party_libxslt)
   os.mkdir(third_party_libxslt)
   files_to_preserve_with_paths = map(
     lambda s: os.path.join(third_party_libxslt, s),
     files_to_preserve)
-  subprocess.check_call(['git', 'reset', '--'] + files_to_preserve_with_paths)
-  subprocess.check_call(['git', 'checkout', '--'] +
-                        files_to_preserve_with_paths)
+  git('reset', '--', *files_to_preserve_with_paths)
+  git('checkout', '--', *files_to_preserve_with_paths)
 
   # Update the libxslt repo and export it to the Chromium tree
   os.chdir(config[libxslt_path])
-  subprocess.check_call(['git', 'remote', 'update', 'origin'])
+  git('remote', 'update', 'origin')
   commit = subprocess.check_output(['git', 'log', '-n', '1',
                                     '--pretty=format:%H', 'origin/master'])
   subprocess.check_call(('git archive origin/master | tar -x -C "%s"' %
@@ -86,22 +94,46 @@ def roll_libxslt(config):
 
   # Add *everything* and push it to the cloud for configuring on Mac, Windows
   os.chdir(full_path_to_third_party_libxslt)
-  subprocess.check_call(['git', 'add', '*'])
-  subprocess.check_call(['git', 'commit', '-m', '%s linux' % commit])
-  subprocess.check_call(['git', 'push', '-f', 'wip',
-                         'HEAD:%s' % config[wip_ref]])
+  git('add', '*')
+  git('commit', '-m', '%s linux' % commit)
+  git('push', '-f', 'wip', 'HEAD:%s' % config[wip_ref])
 
   print('Now run steps on Windows.')
   # TODO: pull files back from Windows
   # TODO: shutil.rmtree unwanted files from libxslt
 
+def roll_libxslt_windows(config):
+  # Fetch the in-progress roll from the experimental branch.
+  os.chdir(config[src_path_windows])
+  git('fetch', 'wip', config[wip_ref])
+  git('reset', '--hard', 'FETCH_HEAD')
 
-def get_out_of_jail(config):
-  os.chdir(config[linux_src_path])
-  subprocess.check_call(['git', 'reset', '--hard', 'origin/master'])
+  # Run the configure script.
+  os.chdir(os.path.join(third_party_libxslt, 'win32'))
+  subprocess.check_call([
+    'cscript', '//E:jscript', 'configure.js', 'compiler=msvc', 'iconv=no',
+    'xslt_debug=no', 'mem_debug=no', 'debugger=no', 'modules=no'
+  ])
 
-def go():
-  roll_libxslt(config)
+  # Add, commit and push the result.
+  os.chdir(os.path.join(config[src_path_windows], third_party_libxslt))
+  shutil.move('config.h', 'win32')
+  git('add', '*')
+  git('commit', '-m', 'Windows')
+  git('push', 'wip', 'HEAD:%s' % config[wip_ref])
 
-def uhoh():
-  get_out_of_jail(config)
+def get_out_of_jail(config, which):
+  os.chdir(config[which])
+  git('reset', '--hard', 'origin/master')
+
+def lgo():
+  roll_libxslt_linux(config)
+
+def luhoh():
+  get_out_of_jail(config, src_path_linux)
+
+def wgo():
+  roll_libxslt_windows(config)
+
+def wuhoh():
+  get_out_of_jail(config, src_path_windows)
